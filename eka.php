258e90de6bb6ad5676af0c46b18a8200a60cafa3
Unknown <tommi.haho@gmail.com>
2017-11-01 11:01:04 +0200

    ensimmäinen array

--- eka.php
+++ eka.php
@@ -5,6 +5,20 @@
     <title>Ekasivu</title>
   </head>
   <body>
-    <h1>Otsikko</h1>
+    <h1>Assosiatiivinen array</h1>
+    <p>Seuraava taulukko on luotu php:llä</p>
+
+    <?php
+    //Alla siis assosiatiivinen array
+    $nimet=array(
+      array("Etunimi"=>'Ville',"Sukunimi"=>'Virtanen'),
+      array("Etunimi"=>'Kalle',"Sukunimi"=>'Järvi'),
+      array("Etunimi"=>'Liisa',"Sukunimi"=>'Virta'),
+      array("Etunimi"=>'Maija',"Sukunimi"=>'Joki')
+    );
+    foreach ($nimet as $rivi => $value) {
+      echo $rivi['Etunimi'];
+    }
+     ?>
   </body>
 </html>
